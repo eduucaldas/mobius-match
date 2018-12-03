4701a4c3cf1c02d24172233788244f1fe253c07f
PioLeErrant <orhan.pierre.france@gmail.com>
2018-12-03 20:07:33 +0100

    Started and finished Mobius Voting, creating Correspondence Processor class for the fuzzy correspondence matrix creation.

--- src/CorrespondenceProcessor.java
+++ src/CorrespondenceProcessor.java
@@ -8,13 +8,38 @@ public class CorrespondenceProcessor {
         throw new Error("to be implemented");
     }
 
+    private double[] computeFeatureVector(Vertex[] selectedSet,Vertex p){
+        /* Computes feature Vector of p with the selected Set.
+         * It is defined as double of length that of selectedSet
+         * it ith element is the geodesic distance to vertex p
+         */
+       throw new Error("to be implemented");
+   }
+   private double computeL2Dist(double[] a){
+        /*L2 distance*/
+       throw new Error("to be implemented");
+   }
+   private double computeFeatureDist(double[] a,double[] b){
+        /* L2 feature distance from a-b*/
+       throw new Error("to be implemented");
+   }
 
     public Vertex[][] computeFuzzyCorrespondenceMatrix(double[][] correspondenceMatrix) {
         /* This function first compute the correspondence matrix using previous algorithm
-         * Then it computes the FUZZY correspondence matrix.
+         * Then it computes the FUZZY correspondence matrix, with a minimum threshold of confidence
+         * We sort the confidence scores.
+         * It then look at low scores, in confidence order and:
+         * For each pair (zj,wj):
+          * 1) computes features vector of zj => aj [see def of this vector in **** func]
+         *  2) Look for closest feature vector of aj => bl, if geodesic dist of this vector to its closest feature vector is smaller than delta (function of surface),
+         *  then we do the same for wj and if it is also true for wj, then we add (zj,wj) to our list of final correspondence.
          * Gives back vertexes of final list of correspondences.
          */
 
+
+
         throw new Error("to be implemented");
     }
+
+
 }
