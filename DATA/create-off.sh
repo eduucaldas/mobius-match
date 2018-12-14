e4976ef3a44fb8c21496f536c25787560c74815b
Eduardo Caldas <eduucaldas@gmail.com>
2018-12-14 12:43:08 +0100

    fix tranform to off

--- DATA/create-off.sh
+++ DATA/create-off.sh
@@ -4,8 +4,11 @@ VERTF=$DATADIR$1".vert"
 TRIF=$DATADIR$1".tri"
 OFFF=$OFFDIR$1".off"
 
-touch $OFFF  
-nLines=$(wc -l < $VERTF)" "$(wc -l < $TRIF) 
-echo $(wc -l < $VERTF)" "$(wc -l < $TRIF) > $OFFF
+touch $OFFF
+echo "OFF" > $OFFF
+echo $(wc -l < $VERTF)" "$(wc -l < $TRIF)" 0" >> $OFFF
 
-cat $VERTF $TRIF >> $OFFF
+cat $VERTF >> $OFFF
+echo "" >> $OFFF
+
+awk '{for(i=1;i<=NF;i++){$i=$i-1}}1' $TRIF | awk '$0="3 "$0' >> $OFFF
